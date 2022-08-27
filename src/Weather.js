a02b27d89ffb614a7eb8e092fd111bba926304e1
Olha Abrosimova <abrosimova2020@ukr.net>
2022-08-27 13:11:01 +0300

    add axios to show weather

---
+++ src/Weather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+import { Bars } from "react-loader-spinner";
+
+export default function Weather(props) {
+	function handleResponse(response) {
+		alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+	}
+	let apiKey = `a2e58143d5353df7726302c2856b0fb6`;
+	let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+	axios.get(urlApi).then(handleResponse);
+
+	return (
+		<div>
+			<Audio
+				height="80"
+				width="80"
+				radius="9"
+				color="green"
+				ariaLabel="three-dots-loading"
+				wrapperStyle
+				wrapperClass
+			/>
+			<Bars
+				height="80"
+				width="80"
+				color="#4fa94d"
+				ariaLabel="bars-loading"
+				wrapperStyle={{}}
+				wrapperClass=""
+				visible={true}
+			/>
+		</div>
+	);
+}
